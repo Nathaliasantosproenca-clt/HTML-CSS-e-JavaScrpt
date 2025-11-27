0bfbe86d6956816edea34aeb8b7e3e03523baefd
Nath_fofi <nathalia.santos.proenca@escola.pr.gov.br>
2025-11-27 08:21:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const avança = document.querySelectorAll('.btn-proximo');
 avança.forEach(button => {
-   button.addEventListener('click', function(){
-    const atual = document.querySelector('.ativo')
-    const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
-    atual.classList.remove('ativo')
-    document.getElementById(proximoPasso).classList.add('ativo')
+   button.addEventListener('click', function () {
+      const atual = document.querySelector('.ativo')
+      const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+      atual.classList.remove('ativo')
+      document.getElementById(proximoPasso).classList.add('ativo')
    })
 })
